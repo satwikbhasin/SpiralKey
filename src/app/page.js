197b338c38ae59908bf9c8e5548cbb3c878e294d
Satwik Bhasin <satwikbhasin@gmail.com>
2024-04-28 17:23:50 -0400

    UI development for encryption & decryption

--- src/app/page.js
+++ src/app/page.js
@@ -1,69 +1,55 @@
 "use client";
-import {
-  AppBar,
-  Toolbar,
-  Typography,
-  TextareaAutosize,
-  Box,
-  Button,
-} from "@mui/material";
-import LockIcon from "@mui/icons-material/LockOutlined";
+import { AppBar, Toolbar, Typography, Box } from "@mui/material";
 import "./globals.css";
-import { useState, useEffect } from "react";
+import { useState } from "react";
+import { Button } from "@mui/material";
+import dynamic from "next/dynamic";
+import CircularProgress from "@mui/material/CircularProgress";
+
+const Loader = () => (
+  <Box
+    sx={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100%",
+      mt: 20,
+    }}
+  >
+    <CircularProgress sx={{ color: "#fa9c2f" }} />
+  </Box>
+);
+const Encryption = dynamic(() => import("@/components/encryption"), {
+  loading: () => <Loader />,
+});
+
+const Decryption = dynamic(() => import("@/components/decryption"), {
+  loading: () => <Loader />,
+});
 
 export default function Home() {
-  const [plaintext, setPlaintext] = useState("");
-
-  const EncryptionState = {
-    IDLE: "idle",
-    IN_PROGRESS: "in-progress",
-    DONE: "done",
+  const AppMode = {
+    ENCRYPTION: "encryption",
+    DECRYPTION: "decryption",
   };
-
-  const [symmetricEncryption, setSymmetricEncryption] = useState(
-    EncryptionState.IDLE
-  );
-  const [spiralEncrypting, setSpiralEncrypting] = useState(
-    EncryptionState.IDLE
-  );
-  function handleEncrypt() {
-    if (plaintext.trim() === "") {
-      return;
-    }
-    setSymmetricEncryption(EncryptionState.IN_PROGRESS);
-    setSpiralEncrypting(EncryptionState.IN_PROGRESS);
-    setTimeout(() => {
-      setSymmetricEncryption(EncryptionState.DONE);
-      setSpiralEncrypting(EncryptionState.DONE);
-    }, 5000);
-    console.log("Encrypting", plaintext);
-  }
-
-  useEffect(() => {
-    console.log("Symmetric Encryption Status", symmetricEncryption);
-    console.log("Spiral Encryption Status", spiralEncrypting);
-  }, [symmetricEncryption, spiralEncrypting]);
-
+  const [mode, setMode] = useState(AppMode.ENCRYPTION);
   return (
     <div
-      style={{ backgroundColor: "#1f1f1f", height: "100vh", width: "100vw" }}
+      style={{
+        backgroundColor: "#1f1f1f",
+        minHeight: "100vh",
+        width: "100%",
+        padding: 50,
+      }}
     >
-      <AppBar position="static" elevation={0}>
+      <AppBar position="absolute" elevation={0}>
         <Toolbar
           sx={{
             justifyContent: "center",
             backgroundColor: "#181818",
           }}
         >
-          <Typography
-            variant="h3"
-            sx={{
-              fontFamily: "'Jersey 15', sans-serif",
-              fontWeight: "400",
-              letterSpacing: "0.1em",
-            }}
-            className="gradient"
-          >
+          <Typography variant="h3" className="gradient jersey-15">
             SpiralKey
           </Typography>
         </Toolbar>
@@ -71,26 +57,21 @@ export default function Home() {
       <Box>
         <Typography
           variant="h3"
+          className="jersey-15"
           sx={{
             textAlign: "center",
             mt: 10,
-            fontFamily: "'Jersey 15', sans-serif",
-            fontWeight: "400",
-            letterSpacing: "0.1em",
           }}
         >
           Welcome to SpiralKey
         </Typography>
         <Typography
           variant="h5"
+          className="gradient jersey-15"
           sx={{
             textAlign: "center",
             mt: 2,
-            fontFamily: "'Jersey 15', sans-serif",
-            fontWeight: "400",
-            letterSpacing: "0.1em",
           }}
-          className="gradient"
         >
           <span>Symmetric Key</span>
           <span> + </span>
@@ -98,77 +79,41 @@ export default function Home() {
           Encryption
         </Typography>
       </Box>
-      <Box>
-        <Box
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          mt: 5,
+        }}
+      >
+        <Button
+          onClick={() => setMode(AppMode.ENCRYPTION)}
           sx={{
-            overflow: "auto",
-            mt: 5,
-            ml: 5,
-            mr: 5,
+            mr: 2,
+            backgroundColor: "",
+            color: mode === AppMode.ENCRYPTION ? "#fa9c2f" : "#6c6c74",
+            textTransform: "lowercase",
+            fontSize: "1.4rem",
           }}
+          className="jersey-15"
         >
-          <Typography
-            variant="h6"
-            sx={{
-              textAlign: "left",
-              mb: 1,
-              fontFamily: "'Jersey 15', sans-serif",
-              fontWeight: "400",
-              letterSpacing: "0.1em",
-            }}
-          >
-            Plain Text
-          </Typography>
-          <TextareaAutosize
-            value={plaintext}
-            onChange={(e) => setPlaintext(e.target.value)}
-            disabled={symmetricEncryption === EncryptionState.IN_PROGRESS}
-            onKeyDown={(e) => {
-              if (e.key === "Enter" && plaintext.trim() !== "") {
-                handleEncrypt();
-                e.preventDefault();
-              }
-            }}
-            style={{
-              width: "100%",
-              overflow: "auto",
-              color: "#6c6c74",
-              backgroundColor: "#1f1f1f",
-              border: "1px solid #6c6c74",
-              padding: 10,
-            }}
-          />
-          <Button
-            variant="contained"
-            startIcon={<LockIcon />}
-            onClick={() => {
-              handleEncrypt();
-            }}
-            disabled={
-              !plaintext.trim() ||
-              symmetricEncryption === EncryptionState.IN_PROGRESS
-            }
-            sx={{
-              background: "#8d5380",
-              mt: 1,
-              "&:hover": {
-                background: !plaintext.trim() ? "#888" : "#adbcfc",
-              },
-            }}
-          >
-            <Typography
-              variant="body"
-              sx={{
-                fontFamily: "'Jersey 15', sans-serif",
-                fontWeight: "400",
-                letterSpacing: "0.1em",
-              }}
-            >
-              Encrypt
-            </Typography>
-          </Button>
-        </Box>
+          Encryption
+        </Button>
+        <Button
+          sx={{
+            mr: 2,
+            backgroundColor: "",
+            color: mode === AppMode.DECRYPTION ? "#fa9c2f" : "#6c6c74",
+            textTransform: "lowercase",
+            fontSize: "1.4rem",
+          }}
+          onClick={() => setMode(AppMode.DECRYPTION)}
+          className="jersey-15"
+        >
+          Decryption
+        </Button>
       </Box>
+      {mode === AppMode.ENCRYPTION ? <Encryption /> : <Decryption />}
     </div>
   );
 }
---
+++ src/components/encryption.jsx
@@ -0,0 +1,449 @@
+"use client";
+import { Typography, TextareaAutosize, Box, Button } from "@mui/material";
+import LockIcon from "@mui/icons-material/LockOutlined";
+import CircularProgress from "@mui/material/CircularProgress";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import IconButton from "@mui/material/IconButton";
+import FileCopyIcon from "@mui/icons-material/FileCopy";
+import "../app/globals.css";
+import { useState, useEffect } from "react";
+
+export default function Encryption() {
+  const [plaintext, setPlaintext] = useState("");
+  const [symmetricCipher, setSymmetricCipher] = useState("");
+  const [spiralCipher, setSpiralCipher] = useState("");
+  const [finalCipher, setFinalCipher] = useState("");
+  const [key1, setKey1] = useState("");
+  const [key2, setKey2] = useState("");
+
+  const [cipherCopied, setCipherCopied] = useState(false);
+  const [key1Copied, setKey1Copied] = useState(false);
+  const [key2Copied, setKey2Copied] = useState(false);
+
+  const EncryptionState = {
+    IDLE: "idle",
+    IN_PROGRESS: "in-progress",
+    DONE: "done",
+  };
+  const [symmetricEncryptionStatus, setSymmetricEncryptionStatus] = useState(
+    EncryptionState.IDLE
+  );
+  const [spiralEncryptionStatus, setSpiralEncryptionStatus] = useState(
+    EncryptionState.IDLE
+  );
+
+  function handleEncrypt() {
+    if (plaintext.trim() === "") {
+      return;
+    }
+    console.log("Encrypting", plaintext);
+    setSymmetricEncryptionStatus(EncryptionState.IN_PROGRESS);
+    setTimeout(() => {
+      setSymmetricEncryptionStatus(EncryptionState.DONE);
+      setSymmetricCipher("DUMMY_SYMMETRIC_CIPHER");
+      setKey1("DUMMY_KEY_1");
+      setKey2("DUMMY_KEY_2");
+      setSpiralEncryptionStatus(EncryptionState.IN_PROGRESS);
+      setTimeout(() => {
+        setSpiralEncryptionStatus(EncryptionState.DONE);
+        setSpiralCipher("DUMMY_SPIRAL_CIPHER");
+        setFinalCipher(spiralCipher);
+      }, 3000);
+    }, 3000);
+  }
+
+  useEffect(() => {
+    console.log("Symmetric Encryption Status", symmetricEncryptionStatus);
+    console.log("Spiral Encryption Status", spiralEncryptionStatus);
+  }, [symmetricEncryptionStatus, spiralEncryptionStatus]);
+
+  useEffect(() => {
+    setSymmetricEncryptionStatus(EncryptionState.IDLE);
+    setSpiralEncryptionStatus(EncryptionState.IDLE);
+    setFinalCipher("");
+  }, [plaintext]);
+
+  return (
+    <div>
+      <Box
+        sx={{
+          overflow: "auto",
+          mt: 5,
+          ml: 5,
+          mr: 5,
+          justifyContent: "center",
+          alignContent: "center",
+          alignItems: "center",
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <Typography
+          variant="h6"
+          className="jersey-15"
+          sx={{
+            textAlign: "center",
+            mb: 1,
+          }}
+        >
+          Plain Text
+        </Typography>
+        <TextareaAutosize
+          value={plaintext}
+          onChange={(e) => setPlaintext(e.target.value)}
+          disabled={symmetricEncryptionStatus === EncryptionState.IN_PROGRESS}
+          onKeyDown={(e) => {
+            if (e.key === "Enter" && plaintext.trim() !== "") {
+              handleEncrypt();
+              e.preventDefault();
+            }
+          }}
+          style={{
+            width: "100%",
+            overflow: "auto",
+            color: "#6c6c74",
+            backgroundColor: "#1f1f1f",
+            border: "1px solid #6c6c74",
+            padding: 10,
+            borderRadius: 5,
+          }}
+        />
+        <Button
+          variant="contained"
+          startIcon={<LockIcon />}
+          onClick={() => {
+            handleEncrypt();
+          }}
+          disabled={
+            !plaintext.trim() ||
+            symmetricEncryptionStatus === EncryptionState.IN_PROGRESS
+          }
+          sx={{
+            background: "#8d5380",
+            mt: 3,
+            "&:hover": {
+                background: "#fa9c2f",
+              },
+          }}
+        >
+          <Typography variant="body" className="jersey-15">
+            Encrypt
+          </Typography>
+        </Button>
+      </Box>
+      <Box
+        sx={{
+          justifyContent: "center",
+          display: "flex",
+          mt: 5,
+        }}
+      >
+        {symmetricEncryptionStatus === EncryptionState.IN_PROGRESS && (
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+            }}
+          >
+            <Typography
+              variant="h6"
+              className="jersey-15"
+              sx={{ color: "#fff", mr: 1 }}
+            >
+              Symmetric Encryption
+            </Typography>
+            <CircularProgress sx={{ color: "#fa9c2f" }} size="20px" />
+          </Box>
+        )}
+
+        {symmetricEncryptionStatus === EncryptionState.DONE && (
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              width: "80%",
+              height: "200px",
+            }}
+          >
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+              }}
+            >
+              <Typography
+                variant="h6"
+                className="jersey-15"
+                sx={{ color: "#fff", mr: 1 }}
+              >
+                Symmetric Encryption
+              </Typography>
+              <CheckCircleIcon fontSize="small" />
+            </Box>
+            <Box
+              sx={{
+                width: "100%",
+                height: "100%",
+                justifyContent: "center",
+                alignItems: "center",
+                display: "flex",
+                padding: 5,
+                borderRadius: 5,
+                border: "1px solid #6c6c74",
+                overflow: "scroll",
+              }}
+            >
+              <Typography
+                variant="body"
+                sx={{
+                  color: "#6c6c74",
+                  height: "100%",
+                  width: "100%",
+                  whiteSpace: "pre-wrap",
+                  wordWrap: "break-word",
+                }}
+              >
+                {symmetricCipher}
+              </Typography>
+            </Box>
+          </Box>
+        )}
+      </Box>
+      <Box
+        sx={{
+          justifyContent: "center",
+          display: "flex",
+          mt: 5,
+        }}
+      >
+        {spiralEncryptionStatus === EncryptionState.IN_PROGRESS && (
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+            }}
+          >
+            <Typography
+              variant="h6"
+              className="jersey-15"
+              sx={{ color: "#fff", mr: 1 }}
+            >
+              Spiral Encryption
+            </Typography>
+            <CircularProgress sx={{ color: "#fa9c2f" }} size="20px" />
+          </Box>
+        )}
+        {symmetricEncryptionStatus === EncryptionState.DONE &&
+          spiralEncryptionStatus === EncryptionState.DONE && (
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                width: "80%",
+                height: "200px",
+              }}
+            >
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "row",
+                  alignItems: "center",
+                }}
+              >
+                <Typography
+                  variant="h6"
+                  className="jersey-15"
+                  sx={{ color: "#fff", mr: 1 }}
+                >
+                  Spiral Encryption
+                </Typography>
+                <CheckCircleIcon fontSize="small" />
+              </Box>
+              <Box
+                sx={{
+                  width: "100%",
+                  height: "100%",
+                  padding: 3,
+                  borderRadius: 5,
+                  border: "1px solid #6c6c74",
+                  overflow: "scroll",
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <Typography
+                  variant="body"
+                  sx={{
+                    color: "#6c6c74",
+                    height: "100%",
+                    width: "100%",
+                    whiteSpace: "pre-wrap",
+                    wordWrap: "break-word",
+                  }}
+                >
+                  {spiralCipher}
+                </Typography>
+              </Box>
+            </Box>
+          )}
+      </Box>
+      <Box
+        sx={{
+          justifyContent: "center",
+          alignContent: "center",
+          alignItems: "center",
+          display: "flex",
+          flexDirection: "column",
+          mt: 5,
+        }}
+      >
+        {symmetricEncryptionStatus === EncryptionState.DONE &&
+          spiralEncryptionStatus === EncryptionState.DONE && (
+            <Box>
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "row",
+                  alignItems: "center",
+                  mb: 5,
+                }}
+              >
+                <Typography
+                  variant="h6"
+                  className="jersey-15"
+                  sx={{
+                    textAlign: "center",
+                    mb: 1,
+                    mr: 1,
+                  }}
+                >
+                  Generated Cipher =
+                </Typography>
+                <TextareaAutosize
+                  value={finalCipher}
+                  disabled
+                  maxRows={3}
+                  style={{
+                    flex: 1,
+                    overflow: "auto",
+                    color: "#6c6c74",
+                    backgroundColor: "#1f1f1f",
+                    border: "1px solid #6c6c74",
+                    padding: 10,
+                    minWidth: "50vw",
+                    borderRadius: 5,
+                  }}
+                />
+                <IconButton
+                  onClick={() => {
+                    navigator.clipboard.writeText(finalCipher);
+                    setCipherCopied(true);
+                    setTimeout(() => setCipherCopied(false), 3000);
+                  }}
+                  sx={{ marginLeft: 1, color: "#fff" }}
+                >
+                  {cipherCopied ? <CheckCircleIcon /> : <FileCopyIcon />}
+                </IconButton>
+              </Box>
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "row",
+                  alignItems: "center",
+                  mb: 5,
+                }}
+              >
+                <Typography
+                  variant="h6"
+                  className="jersey-15"
+                  sx={{
+                    textAlign: "center",
+                    mb: 1,
+                    mr: 1,
+                  }}
+                >
+                  Key 1 =
+                </Typography>
+                <TextareaAutosize
+                  value={key1}
+                  disabled
+                  maxRows={3}
+                  style={{
+                    flex: 1,
+                    overflow: "auto",
+                    color: "#6c6c74",
+                    backgroundColor: "#1f1f1f",
+                    border: "1px solid #6c6c74",
+                    padding: 10,
+                    minWidth: "50vw",
+                    borderRadius: 5,
+                  }}
+                />
+                <IconButton
+                  onClick={() => {
+                    navigator.clipboard.writeText(key1);
+                    setKey1Copied(true);
+                    setTimeout(() => setKey1Copied(false), 3000);
+                  }}
+                  sx={{ marginLeft: 1, color: "#fff" }}
+                >
+                  {key1Copied ? <CheckCircleIcon /> : <FileCopyIcon />}
+                </IconButton>
+              </Box>
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "row",
+                  alignItems: "center",
+                  mb: 5,
+                }}
+              >
+                <Typography
+                  variant="h6"
+                  className="jersey-15"
+                  sx={{
+                    textAlign: "center",
+                    mb: 1,
+                    mr: 1,
+                  }}
+                >
+                  Key 2 =
+                </Typography>
+                <TextareaAutosize
+                  value={key2}
+                  disabled
+                  maxRows={3}
+                  style={{
+                    flex: 1,
+                    overflow: "auto",
+                    color: "#6c6c74",
+                    backgroundColor: "#1f1f1f",
+                    border: "1px solid #6c6c74",
+                    padding: 10,
+                    minWidth: "50vw",
+                    borderRadius: 5,
+                  }}
+                />
+                <IconButton
+                  onClick={() => {
+                    navigator.clipboard.writeText(key2);
+                    setKey2Copied(true);
+                    setTimeout(() => setKey2Copied(false), 3000);
+                  }}
+                  sx={{ marginLeft: 1, color: "#fff" }}
+                >
+                  {key2Copied ? <CheckCircleIcon /> : <FileCopyIcon />}
+                </IconButton>
+              </Box>
+            </Box>
+          )}
+      </Box>
+    </div>
+  );
+}
