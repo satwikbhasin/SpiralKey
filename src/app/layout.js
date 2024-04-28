c28402f1940dd4c29fed77de051b93198fd2b18c
Satwik Bhasin <satwikbhasin@gmail.com>
2024-04-28 12:51:42 -0400

    plaintext_input_ui

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,7 +13,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link
           href="https://fonts.googleapis.com/css2?family=Jersey+15&display=swap"
           rel="stylesheet"
--- src/app/page.js
+++ src/app/page.js
@@ -1,31 +1,174 @@
-import React from "react";
-import AppBar from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
+"use client";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  TextareaAutosize,
+  Box,
+  Button,
+} from "@mui/material";
+import LockIcon from "@mui/icons-material/LockOutlined";
+import "./globals.css";
+import { useState, useEffect } from "react";
 
 export default function Home() {
+  const [plaintext, setPlaintext] = useState("");
+
+  const EncryptionState = {
+    IDLE: "idle",
+    IN_PROGRESS: "in-progress",
+    DONE: "done",
+  };
+
+  const [symmetricEncryption, setSymmetricEncryption] = useState(
+    EncryptionState.IDLE
+  );
+  const [spiralEncrypting, setSpiralEncrypting] = useState(
+    EncryptionState.IDLE
+  );
+  function handleEncrypt() {
+    if (plaintext.trim() === "") {
+      return;
+    }
+    setSymmetricEncryption(EncryptionState.IN_PROGRESS);
+    setSpiralEncrypting(EncryptionState.IN_PROGRESS);
+    setTimeout(() => {
+      setSymmetricEncryption(EncryptionState.DONE);
+      setSpiralEncrypting(EncryptionState.DONE);
+    }, 5000);
+    console.log("Encrypting", plaintext);
+  }
+
+  useEffect(() => {
+    console.log("Symmetric Encryption Status", symmetricEncryption);
+    console.log("Spiral Encryption Status", spiralEncrypting);
+  }, [symmetricEncryption, spiralEncrypting]);
+
   return (
-    <div>
-      <AppBar position="static">
+    <div
+      style={{ backgroundColor: "#1f1f1f", height: "100vh", width: "100vw" }}
+    >
+      <AppBar position="static" elevation={0}>
         <Toolbar
           sx={{
             justifyContent: "center",
-            backgroundColor: "#702de0",
+            backgroundColor: "#181818",
           }}
         >
           <Typography
             variant="h3"
-            color="inherit"
             sx={{
               fontFamily: "'Jersey 15', sans-serif",
               fontWeight: "400",
               letterSpacing: "0.1em",
             }}
+            className="gradient"
           >
             SpiralKey
           </Typography>
         </Toolbar>
       </AppBar>
+      <Box>
+        <Typography
+          variant="h3"
+          sx={{
+            textAlign: "center",
+            mt: 10,
+            fontFamily: "'Jersey 15', sans-serif",
+            fontWeight: "400",
+            letterSpacing: "0.1em",
+          }}
+        >
+          Welcome to SpiralKey
+        </Typography>
+        <Typography
+          variant="h5"
+          sx={{
+            textAlign: "center",
+            mt: 2,
+            fontFamily: "'Jersey 15', sans-serif",
+            fontWeight: "400",
+            letterSpacing: "0.1em",
+          }}
+          className="gradient"
+        >
+          <span>Symmetric Key</span>
+          <span> + </span>
+          <span>Spiral Matrix </span>
+          Encryption
+        </Typography>
+      </Box>
+      <Box>
+        <Box
+          sx={{
+            overflow: "auto",
+            mt: 5,
+            ml: 5,
+            mr: 5,
+          }}
+        >
+          <Typography
+            variant="h6"
+            sx={{
+              textAlign: "left",
+              mb: 1,
+              fontFamily: "'Jersey 15', sans-serif",
+              fontWeight: "400",
+              letterSpacing: "0.1em",
+            }}
+          >
+            Plain Text
+          </Typography>
+          <TextareaAutosize
+            value={plaintext}
+            onChange={(e) => setPlaintext(e.target.value)}
+            disabled={symmetricEncryption === EncryptionState.IN_PROGRESS}
+            onKeyDown={(e) => {
+              if (e.key === "Enter" && plaintext.trim() !== "") {
+                handleEncrypt();
+                e.preventDefault();
+              }
+            }}
+            style={{
+              width: "100%",
+              overflow: "auto",
+              color: "#6c6c74",
+              backgroundColor: "#1f1f1f",
+              border: "1px solid #6c6c74",
+              padding: 10,
+            }}
+          />
+          <Button
+            variant="contained"
+            startIcon={<LockIcon />}
+            onClick={() => {
+              handleEncrypt();
+            }}
+            disabled={
+              !plaintext.trim() ||
+              symmetricEncryption === EncryptionState.IN_PROGRESS
+            }
+            sx={{
+              background: "#8d5380",
+              mt: 1,
+              "&:hover": {
+                background: !plaintext.trim() ? "#888" : "#adbcfc",
+              },
+            }}
+          >
+            <Typography
+              variant="body"
+              sx={{
+                fontFamily: "'Jersey 15', sans-serif",
+                fontWeight: "400",
+                letterSpacing: "0.1em",
+              }}
+            >
+              Encrypt
+            </Typography>
+          </Button>
+        </Box>
+      </Box>
     </div>
   );
 }
